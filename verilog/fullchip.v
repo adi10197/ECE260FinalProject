241c8d6fbb81bdbc8ec68afa37abc7bdd13c3291
Adithya Anand <n.adithya10197@gmail.com>
2022-03-14 23:32:14 -0700

    no external fifos

--- verilog/fullchip.v
+++ verilog/fullchip.v
@@ -17,48 +17,26 @@ input  reset;
 // out of the cores
 wire [bw-1:0] sum_out_0, sum_out_1;
 
-// out of the synchronizers
-wire [bw-1:0] sum_out_sync_0, sum_out_sync_1;
-
 core #(.bw(bw), .bw_psum(bw_psum), .col(col), .pr(pr)) core_instance_0 (
       .reset(reset), 
-      .clk(clk), 
+      .clk(clk0), 
       .mem_in(mem_in_0), 
       .inst(inst),
 
+      .otherCoreClk(clk1)
       .sum_out(sum_out_0),
-      .sum_in(sum_out_sync_1)
+      .sum_in(sum_out_1)
 );
 
 core #(.bw(bw), .bw_psum(bw_psum), .col(col), .pr(pr)) core_instance_1 (
       .reset(reset), 
-      .clk(clk), 
+      .clk(clk1), 
       .mem_in(mem_in_1), 
       .inst(inst),
       
+      .otherCoreClk(clk0)
       .sum_out(sum_out_1),
-      .sum_in(sum_out_sync_0)
-);
-
-// figure out rd/ wr signals
-fifo_depth16 #(.bw(bw_psum+4)) sync_core_0 (
-      .rd_clk(clk0), 
-      .wr_clk(clk1), 
-      .in(sum_out_0),
-      .out(sum_out_sync_1), 
-      .rd(div_q), 
-      .wr(fifo_wr), 
-      .reset(reset)
-);
-
-fifo_depth16 #(.bw(bw_psum+4)) sync_core_1 (
-      .rd_clk(clk1), 
-      .wr_clk(clk0), 
-      .in(sum_out_1),
-      .out(sum_out_sync_0), 
-      .rd(div_q), 
-      .wr(fifo_wr), 
-      .reset(reset)
+      .sum_in(sum_out_0)
 );
 
 endmodule
