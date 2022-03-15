941ca4783f83f111bc7d2dba743a3ccbb05e2d49
Adithya Anand <n.adithya10197@gmail.com>
2022-03-14 23:48:39 -0700

    synchronizer

--- verilog/fullchip.v
+++ verilog/fullchip.v
@@ -14,6 +14,11 @@ input  [pr*bw-1:0] mem_in_1;
 input  [16:0] inst; 
 input  reset;
 
+reg sync0_0, sync0_1, sync0_2;
+reg sync1_0, sync1_1, sync1_2;
+
+wire canRead_0, canRead_1;
+
 // out of the cores
 wire [bw-1:0] sum_out_0, sum_out_1;
 
@@ -23,9 +28,14 @@ core #(.bw(bw), .bw_psum(bw_psum), .col(col), .pr(pr)) core_instance_0 (
       .mem_in(mem_in_0), 
       .inst(inst),
 
+      // additional inputs
       .otherCoreClk(clk1)
+      .sum_in(sum_out_1),
+      .thisCoreCanRead(sync0_2),
+
+      // additional outputs
       .sum_out(sum_out_0),
-      .sum_in(sum_out_1)
+      .otherCoreCanRead(canRead_0)
 );
 
 core #(.bw(bw), .bw_psum(bw_psum), .col(col), .pr(pr)) core_instance_1 (
@@ -34,9 +44,24 @@ core #(.bw(bw), .bw_psum(bw_psum), .col(col), .pr(pr)) core_instance_1 (
       .mem_in(mem_in_1), 
       .inst(inst),
       
+      // additional inputs
       .otherCoreClk(clk0)
+      .sum_in(sum_out_0),
+      .thisCoreCanRead(sync1_2),
+
+      // additional outputs
       .sum_out(sum_out_1),
-      .sum_in(sum_out_0)
+      .otherCoreCanRead(canRead_1)
 );
 
+always @ (posedge clk0) begin
+      sync0_0 <= canRead_1;
+      sync0_1 <= sync0_0;
+      sync0_2 <= sync0_1;
+endmodule
+
+always @ (posedge clk1) begin
+      sync1_0 <= canRead_0;
+      sync1_1 <= sync1_0;
+      sync1_2 <= sync1_1;
 endmodule
